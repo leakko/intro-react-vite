715365cd68a6d35eac3aa205193cd430ff3a709e
marcos.rodriguez3@vodafone.com <marcos.rodriguez3@vodafone.com>
2022-09-05 16:13:34 +0200

    FirstApp component created

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelloWorldApp } from './HelloWorldApp'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelloWorldApp />
+    <FirstApp />
   </React.StrictMode>
 )
