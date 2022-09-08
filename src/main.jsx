58ec6ee96fc77c76c374d852671296f28c95e168
marcos.rodriguez3@vodafone.com <marcos.rodriguez3@vodafone.com>
2022-09-08 15:55:13 +0200

    First useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title="Hola soy Goku" subtitle={123}/>
+    <CounterApp value={5} />
   </React.StrictMode>
 )
