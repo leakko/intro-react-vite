b41bcf2d94c99db9b67bd233a0c26d726026d3a8
marcos.rodriguez3@vodafone.com <marcos.rodriguez3@vodafone.com>
2022-09-12 14:21:39 +0200

    11-async-await test

---
+++ tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,8 @@
+import { getImagen } from "../../src/base-pruebas/11-async-await";
+
+describe('Pruebas en 11-async-await.js', () => {
+    test('getImagen debe retornar una URL de la imagen', async () => {
+        const url = await getImagen();
+        console.log(url);
+    });
+});
